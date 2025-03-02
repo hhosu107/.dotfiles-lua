d8249b40e5ac29787054e1c4f944e7bdae2013a3
Chansu Park <hhosu107@gmail.com>
2025-03-03 01:41:30 +0900

    Add augroup

---
+++ home/config/nvim/lua/utils/augroup.lua
@@ -0,0 +1,33 @@
+-- utils/augroup.lua
+
+
+local M = {}
+
+
+M.setup = function()
+  local augroup = vim.api.nvim_create_augroup("vimrc", { clear = true })
+
+
+  vim.api.nvim_create_autocmd({ "FocusGained", "BufEnter", "CursorHold", "CursorHoldI" }, {
+    group = augroup,
+    pattern = "*",
+    callback = function()
+      if vim.api.nvim_get_mode().mode ~= "c" then
+        vim.cmd("checktime")
+
+      end
+    end,
+  })
+
+
+  vim.api.nvim_create_autocmd("FileChangedShellPost", {
+    group = augroup,
+    pattern = "*",
+    callback = function()
+      vim.cmd("echohl WarningMsg | echo 'File " .. vim.fn.expand("<afile>") .. " changed on disk. Buffer reloaded.' | echohl None")
+    end,
+
+  })
+end
+
+return M
