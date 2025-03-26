f7b01042d25c111f32ac6ef6f869605fd8ad5cb1
Chansu Park <hhosu107@gmail.com>
2025-03-26 17:24:22 +0900

    [SOLVED] RPC[Error] code_name = InternalError, message = "Request textDocument/foldingRange failed with message: Cannot read properties of undefined (reading 'lineFoldingOnly')"
    I found a solution for the above error on yaml-language-server via https://github.com/redhat-developer/yaml-language-server/issues/912

--- home/config/nvim/lua/config/lsp.lua
+++ home/config/nvim/lua/config/lsp.lua
@@ -1,6 +1,11 @@
 require('mason').setup({})
 require('mason-lspconfig').setup()
 
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities.textDocument.foldingRange = {
+    dynamicRegistration = false,
+    lineFoldingOnly = true
+}
 
 require("mason-lspconfig").setup_handlers {
   ["rust_analyzer"] = function ()
@@ -9,7 +14,9 @@ require("mason-lspconfig").setup_handlers {
   -- and will be called for each installed server that doesn't have
   -- a dedicated handler.
   function (server_name) -- default handler (optional)
-    require("lspconfig")[server_name].setup {}
+    require("lspconfig")[server_name].setup {
+      capabilities = capabilities,
+    }
   end,
   -- Next, you can provide a dedicated handler for specific servers.
   -- For example, a handler override for the `rust_analyzer`:
